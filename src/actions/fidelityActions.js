e5790010d8af18fa647c726d741dff74db7c3e6f
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-14 23:56:08 +0200

    redux logic implemented

--- src/actions/fidelityActions.js
+++ src/actions/fidelityActions.js
@@ -65,58 +65,19 @@ function fetchDiscounts(payload) {
     });
     try {
 
-      const discounts = [
-        {
-        "code": "937c15",
-        "reward": "7.02",
-        "reward_currency": "EUR",
-        "views": 3,
-        "date": "2018-05-20T20:35:36.008158Z",
-        "discount": 146
-      }, {
-        "code": "c0d01e",
-        "reward": "37.43",
-        "reward_currency": "EUR",
-        "views": 16,
-        "date": "2018-05-20T20:35:36.069622Z",
-        "discount": 148
-      }, {
-        "code": "4f99dd",
-        "reward": "11.70",
-        "reward_currency": "EUR",
-        "views": 5,
-        "date": "2018-05-20T20:35:36.050409Z",
-        "discount": 148
-      }, {
-        "code": "123940",
-        "reward": "28.07",
-        "reward_currency": "EUR",
-        "views": 12,
-        "date": "2018-05-20T20:35:36.144615Z",
-        "discount": 152
-      }, {
-        "code": "45c5bc",
-        "reward": "2.34",
-        "reward_currency": "EUR",
-        "views": 1,
-        "date": "2018-05-20T20:35:36.126723Z",
-        "discount": 152
-      }
-    ];
-
-      // const informationData = { ...getState().information.informationData
-      // };
-      // const response = await axiosInstance({
-      //   method: "get",
-      //   url: "/customers/discount_activations/",
-      //   params: {
-      //     mac_address: informationData.mac_address
-      //   }
-      // });
+      const informationData = { ...getState().information.informationData
+      };
+      const response = await axiosInstance({
+        method: "get",
+        url: "/customers/discount_activations/",
+        params: {
+          mac_address: informationData.mac_address
+        }
+      });
 
       dispatch({
         type: FETCH_DISCOUNTS_FULFILLED,
-        payload: discounts
+        payload: response.data
       });
     } catch (error) {
       dispatch({
