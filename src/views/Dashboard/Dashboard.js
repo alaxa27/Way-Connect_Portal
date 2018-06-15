e5171875e27e1138d37ec42f63b34d558b5ba032
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-15 02:00:50 +0200

    Dashboard cleansing.

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import {Button, Row, Col} from "reactstrap";
 
+import ActionButton from "./components/ActionButton"
+
 @connect((store) => {
   let informationStore = store.information;
   return {
@@ -14,41 +16,13 @@ import {Button, Row, Col} from "reactstrap";
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-    };
   }
 
-  renderButton(link, icon, text, render) {
-    if (render) {
-      return (<div>
-        <Link to={"/" + link}>
-          <Button>
-            <i className={"fa fa-" + icon}></i>
-          </Button>
-        </Link>
-        <h4>
-          {text}
-        </h4>
-      </div>);
-    } else {
-      return null;
-    }
-
-  }
   render() {
     return (<div className="dashboard">
-      <div>
-        <Button>
-          <i className="fa fa-wifi"></i>
-        </Button>
-        <h4>
-          {"Join the WiFi Network"}
-        </h4>
-      </div>
-      {this.renderButton("claim", "exclamation-triangle", "Make a Claim", this.props.isHotel)}
-      {this.renderButton("fidelity", "hand-holding-usd", "Fidelity Bonus", !this.props.isHotel)}
-
+    <ActionButton link="wifi-access" icon="wifi" text="Join the WiFi Network" show={true} />
+    <ActionButton link="claim" icon="exclamation-triangle" text="Make a Claim" show={this.props.isHotel} />
+    <ActionButton link="fidelity" icon="hand-holding-usd" text="Fidelity Bonus" show={!this.props.isHotel} />
     </div>);
   }
 }
---
+++ src/views/Dashboard/components/ActionButton.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
+import {Button} from "reactstrap";
+
+class ActionButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {link, text, icon, show} = this.props;
+    if (show) {
+      return (<div>
+        <Link to={"/" + link}>
+          <Button>
+            <i className={"fa fa-" + icon}></i>
+          </Button>
+        </Link>
+        <h4>
+          {text}
+        </h4>
+      </div>);
+    } else {
+      return null;
+    }
+  }
+}
+
+ActionButton.propTypes = {
+  show: PropTypes.bool,
+  link: PropTypes.string,
+  text: PropTypes.string,
+  icon: PropTypes.string
+};
+
+export default ActionButton;
