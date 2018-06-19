4019ff99dbf867531aa4e6dcedf3b665fe7d2e2f
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-19 04:40:26 +0200

    Profile first question integration.

--- src/views/Fidelity/Profile/Profile.js
+++ src/views/Fidelity/Profile/Profile.js
@@ -2,10 +2,13 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
-import {Row, Col} from "reactstrap";
+import {Button, Label} from "reactstrap";
 
 import Navbar from "../../../components/Navbar";
 import Loader from "../../../components/Loader";
+import SelectBox from "../../../components/SelectBox";
+
+const STATUS = require("../../../data/status");
 
 // import {fetchFidelity, fetchDiscount} from "../../actions/fidelityActions";
 //
@@ -16,13 +19,36 @@ import Loader from "../../../components/Loader";
 
 class Profile extends Component {
   constructor(props) {
+
     super(props);
-    this.state = {};
+    this.state = {
+      nationality: STATUS["NATIONALITY"]
+    };
+    this.updateNationality = this.updateNationality.bind(this);
+  }
+
+  updateNationality(val) {
+    let userData = {
+      ...this.state.userData
+    };
+    userData.nationality = val;
+    this.setState({userData});
   }
 
   render() {
     return (<div className="profile">
       <Navbar title="Profile" goBack="/fidelity" history={this.props.history}/>
+      <div>
+        <Label>
+          <p>{"Select your"}</p>
+          {"Nationality"}
+        </Label>
+        <SelectBox name="nationality-select" options={this.state.nationality} onChange={this.updateNationality}/>
+      </div>
+      <Button className="next-btn">
+        {"Next Question"}
+        <i className="fa fa-chevron-right"></i>
+      </Button>
     </div>);
   }
 }
