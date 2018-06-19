1caa3bcdb3ad75ed91e1cda809b9bc071f8c1ab3
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-19 19:33:37 +0200

    Questions with progressBar and Congratulations

--- src/views/Fidelity/Profile/Profile.js
+++ src/views/Fidelity/Profile/Profile.js
@@ -2,10 +2,11 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
-import {Button, Label} from "reactstrap";
+import {Button, Progress} from "reactstrap";
 
 import Navbar from "../../../components/Navbar";
 import Loader from "../../../components/Loader";
+import Congratulations from "./components/Congratulations";
 import Question from "./components/Question";
 
 const STATUS = require("../../../data/status");
@@ -22,25 +23,26 @@ class Profile extends Component {
 
     super(props);
     this.state = {
+      id: 0,
       questions: [
         {
           name: "nationality",
           type: "select-unique",
           title: "Nationality",
           options: STATUS["NATIONALITY"],
-          value: {}
+          value: null
         }, {
           name: "work",
           type: "select-unique",
           title: "Work Status",
           options: STATUS["PROFESSIONAL"],
-          value: {}
+          value: null
         }, {
           name: "relationship",
           type: "select-unique",
           title: "Relationship Status",
           options: STATUS["RELATIONSHIP"],
-          value: {}
+          value: null
         }
       ]
     };
@@ -52,24 +54,37 @@ class Profile extends Component {
     let questions = [...this.state.questions];
     const index = questions.findIndex(e => e.name === name);
     questions[index].value = val;
-    this.setState({
-      questions: questions
-    });
+    this.setState({questions: questions});
   }
 
   goToNextQuestion() {
+    if (this.state.questions[this.state.id].value !== null && this.state.id < this.state.questions.length) {
+      this.setState({
+        id: this.state.id + 1
+      });
+    }
     console.log(this.state);
+  }
+
+  _renderQuestion() {
+    if (this.state.id < this.state.questions.length) {
+      return (<div className="question-block">
+        <Question {...this.state.questions[this.state.id]} updateValue={this.updateValue}/>
+        <Button className="next-btn" onClick={this.goToNextQuestion}>
+          {"Next Question"}
+          <i className="fa fa-chevron-right"></i>
+        </Button>
+      </div>);
+    } else {
+      return (<Congratulations/>);
+    }
+
   }
 
   render() {
     return (<div className="profile">
       <Navbar title="Profile" goBack="/fidelity" history={this.props.history}/>
-      <Question {...this.state.questions[0]} updateValue={this.updateValue}/>
-      <Question {...this.state.questions[1]} updateValue={this.updateValue}/>
-      <Button className="next-btn" onClick={this.goToNextQuestion}>
-        {"Next Question"}
-        <i className="fa fa-chevron-right"></i>
-      </Button>
+      <Progress value={(this.state.id / this.state.questions.length) * 100}/> {this._renderQuestion()}
     </div>);
   }
 }
---
+++ src/views/Fidelity/Profile/components/Congratulations.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import {} from "reactstrap";
+
+class Congratulations extends Component {
+  constructor(props) {
+
+    super(props);
+  }
+
+  render() {
+    return (<div>
+      {"Congratulations"}
+    </div>);
+  }
+}
+
+export default Congratulations;
