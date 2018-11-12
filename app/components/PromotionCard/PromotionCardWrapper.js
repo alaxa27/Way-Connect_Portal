a9e477d8a673c9fa048a44e5499764a2e6766869
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-11-12 16:03:03 +0100

    Added shadow to promotionCards

--- app/components/PromotionCard/PromotionCardWrapper.js
+++ app/components/PromotionCard/PromotionCardWrapper.js
@@ -5,12 +5,13 @@ const PromotionCardWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: ${props => (props.locked ? 'center' : 'inherit')};
+  margin: 15px 0;
+  padding: ${props => (props.locked ? '50px' : '15px')};
   background: #3a3a3a;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
   font-size: 0.75rem;
   font-weight: lighter;
   color: ${props => (props.locked && !props.active ? '#666666' : '#cccccc')};
-  margin: 15px 0;
-  padding: ${props => (props.locked ? '50px' : '15px')};
 `;
 
 export default PromotionCardWrapper;
