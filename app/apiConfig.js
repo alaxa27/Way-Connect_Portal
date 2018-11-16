8214a2d05674ffdedf3fc4d8a28db55342898bb1
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-11-16 09:54:20 +0100

    config right backend host in prod.

--- app/apiConfig.js
+++ app/apiConfig.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+let backendHost = 'http://localhost:5000';
+
+if (process.env.NODE_ENV === 'production') {
+  backendHost = 'http://192.168.220.2:5000';
+}
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: backendHost,
 });
 
 export default axiosInstance;
