4f7ec9124942f7e14ddfaa32ba5a14a2cb93c6cb
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-11-19 17:44:58 +0100

    /portal sub domain

--- app/apiConfig.js
+++ app/apiConfig.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 let backendHost = 'http://localhost:5000';
 
 if (process.env.NODE_ENV === 'production') {
-  backendHost = 'http://192.168.220.2:5000/portal';
+  backendHost = 'http://192.168.220.2:5000';
 }
 
 const axiosInstance = axios.create({
-  baseURL: backendHost,
+  baseURL: `${backendHost}/portal`,
 });
 
 export default axiosInstance;
