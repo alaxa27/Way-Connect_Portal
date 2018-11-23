d67d96f51ddaebdd2c5551afb16ea2c88964794c
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-11-23 15:17:30 +0100

    saga loadEstablishment test.

--- app/containers/LoaderPage/tests/saga.test.js
+++ app/containers/LoaderPage/tests/saga.test.js
@@ -4,12 +4,47 @@
 
 /* eslint-disable redux-saga/yield-effects */
 // import { take, call, put, select } from 'redux-saga/effects';
-// import loaderPageSaga from '../saga';
+import { put } from 'redux-saga/effects';
+
+// import { GET_ESTABLISHMENT } from '../constants';
+
+import { establishmentLoaded, establishmentLoadingError } from '../actions';
+import { getEstablishmentEffect } from '../saga';
 
 // const generator = loaderPageSaga();
 
-describe('loaderPageSaga Saga', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+describe('getEstablishment effect', () => {
+  let getEstablishmentGenerator;
+
+  beforeEach(() => {
+    getEstablishmentGenerator = getEstablishmentEffect();
+
+    const callDescriptor = getEstablishmentGenerator.next().value;
+    expect(callDescriptor).toMatchSnapshot();
+  });
+
+  it('should dispatch the establishmentLoaded action if the request is successful', () => {
+    const data = {
+      name: 'foo',
+    };
+    const putDescriptor = getEstablishmentGenerator.next({ data }).value;
+    // console.log(putDescriptor)
+    expect(putDescriptor).toEqual(put(establishmentLoaded(data.name)));
+  });
+
+  it('should dispatch the establishmentLoadingError action requests fails', () => {
+    const response = new Error('Some error');
+    const putDescriptor = getEstablishmentGenerator.throw(response).value;
+    expect(putDescriptor).toEqual(put(establishmentLoadingError(response)));
   });
 });
+// describe('loaderPageSaga Saga', () => {
+//   const loaderPageSaga = loaderPage();
+
+//   it('should start task to watch for GET_ESTABLISHMENT action', () => {
+//     const takeLatestDescriptor = loaderPageSaga.next().value;
+//     expect(takeLatestDescriptor).toEqual(
+//       takeLatest(GET_ESTABLISHMENT, getEstablishmentEffect),
+//     );
+//   });
+// });
