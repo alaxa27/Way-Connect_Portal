aee60f362a2e690d136f0374dcb735e1f040e280
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-11-26 18:06:20 +0100

    Contact component integration done.

---
+++ app/components/Communication/CommunicationTitle.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const CommmunicationTitle = styled.div`
+  color: #666666;
+  font-size: 1rem;
+  font-weight: lighter;
+`;
+
+export default CommmunicationTitle;
--- app/components/Communication/index.js
+++ app/components/Communication/index.js
@@ -5,25 +5,39 @@
  */
 
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 
 import VideoPlayer from 'components/VideoPlayer';
+import PhoneNumber from 'components/PhoneNumber';
 import CommunicationWrapper from './CommunicationWrapper';
+// import CommunicationTitle from './CommunicationTitle';
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
 class Communication extends React.Component {
+  renderPhoneNumber(phoneNumber) {
+    if (phoneNumber)
+      return (
+        <PhoneNumber phoneNumber={phoneNumber}>Contact direct</PhoneNumber>
+      );
+    return null;
+  }
+
   render() {
     return (
       <CommunicationWrapper>
+        {/* <CommunicationTitle>Chanel | Bleu, fragrance homme</CommunicationTitle> */}
+        {this.renderPhoneNumber(this.props.phone_number)}
         <VideoPlayer {...this.props} />
       </CommunicationWrapper>
     );
   }
 }
 
-Communication.propTypes = {};
+Communication.propTypes = {
+  phone_number: PropTypes.string,
+};
 
 export default Communication;
