bb54276d8df8ea47fb9ece5cd7c40c7951776e75
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-12-16 19:22:39 +0100

    Loading component

---
+++ app/components/Loading/LoadingWrapper.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const LoadingWrapper = styled.div`
+  margin: auto;
+`;
+
+export default LoadingWrapper;
---
+++ app/components/Loading/index.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * Loading
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Loader from 'react-loader-spinner';
+
+import LoadingWrapper from './LoadingWrapper';
+// import styled from 'styled-components';
+
+function Loading(props) {
+  return (
+    <LoadingWrapper>
+      <Loader type="Rings" color="#6D6D6D" {...props} />
+    </LoadingWrapper>
+  );
+}
+
+Loading.defaultProps = {
+  height: '100',
+  width: '100',
+};
+
+Loading.propTypes = {
+  height: PropTypes.string,
+  width: PropTypes.string,
+};
+
+export default Loading;
