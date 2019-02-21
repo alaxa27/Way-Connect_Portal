6c11e7df22109e0abed849873575e19fbd18532a
Alaxa <alaxa27@gmail.com>
2019-02-21 21:48:19 +0100

    company_name campaign_name font-size : 1rem -> 0.75rem

--- app/components/Communication/CommunicationTitle.js
+++ app/components/Communication/CommunicationTitle.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const CommmunicationTitle = styled.div`
   color: ${props => props.theme.colors.text};
-  font-size: 1rem;
+  font-size: 0.75rem;
   font-weight: lighter;
 `;
 
